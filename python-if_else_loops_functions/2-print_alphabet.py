61a1495ff69b41ff5b6709d91cd10304678b102d
ST-GuY <estebanlvy23@gmail.com>
2025-09-02 14:55:31 +0200

    print alphabet avec commentaire

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for a in range(97, 123):
+    # Boucle sur les valeurs de 97 à 122 inclus (codes ASCII pour 'a' à 'z')
+
+    print(chr(a), end='')
+    # Convertit le code ASCII en caractère avec chr(a)
+    # et l'affiche sans ajouter de nouvelle ligne (end='')
