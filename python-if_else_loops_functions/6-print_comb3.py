23946ba8dd8434e1cea0afdfecd85c0746466217
ST-GuY <estebanlvy23@gmail.com>
2025-09-03 10:09:44 +0200

    .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for a in range(0, 9):
     for b in range(a + 1, 10):
         # Exemple : si a = 1, b commence à 2 pour éviter les doublons
         if a == 8 and b == 9:
-            print(f"{a}{b}")
+            print("{}{}".format(a, b))
         else:
-            print(f"{a}{b}", end=", ")
+            print("{}{}".format(a, b), end=", ")
