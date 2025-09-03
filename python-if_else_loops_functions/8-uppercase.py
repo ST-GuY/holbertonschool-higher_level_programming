e8a9e4aa0b2f41744c8def78bad25f5f8e436d7b
ST-GuY <estebanlvy23@gmail.com>
2025-09-03 14:23:51 +0200

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for c in str:
-        if ord(c) in range(97, 123):
-            print(chr(ord(c) - 32), end="")
+    result = ""  # Initialisation d'une chaîne vide pour stocker le résultat final
+
+    for c in str:  # Boucle sur chaque caractère 'c' de la chaîne d'entrée
+        if 97 <= ord(c) <= 122:  # (ASCII de 'a' = 97 à 'z' = 122)
+            result += chr(ord(c) - 32)  # Convertit la minuscule en majuscule
         else:
-            print(c, end="")
+            result += c  # Si ce n'est pas une minuscule, on ajoute le caractère tel quel au résultat
+
+    print("{}".format(result))  # Affiche la chaîne finale en majuscules
