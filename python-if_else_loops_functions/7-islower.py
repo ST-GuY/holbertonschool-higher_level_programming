baaf4f5fada13af63d4821803be8a00e98a570e2
ST-GuY <estebanlvy23@gmail.com>
2025-09-03 11:26:49 +0200

    commentaire

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if ord(c) in range(97, 123):
+    if ord(c) in range(97, 123):  # Si c'est une minuscule
         return True
     else:
         return False
