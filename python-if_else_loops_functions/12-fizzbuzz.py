af9b53aeb29e69a9f0f10cc912f4880d148ed828
ST-GuY <estebanlvy23@gmail.com>
2025-09-03 15:31:05 +0200

    pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,9 @@ def fizzbuzz():
     for a in range(1, 101):
         if a % 3 == 0 and a % 5 == 0:  # multiple de 3 et 5
             print("FizzBuzz", end=' ')
-        elif a % 3 == 0: # multiple de 3
+        elif a % 3 == 0:  # multiple de 3
             print("Fizz", end=' ')
-        elif a % 5 == 0: # multiple de 5
+        elif a % 5 == 0:  # multiple de 5
             print("Buzz", end=' ')
         else:
             print(a, end=' ')
