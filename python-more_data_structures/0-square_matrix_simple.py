87523a066ee86d85f024983a4f825e78067beaaf
ST-GuY <estebanlvy23@gmail.com>
2025-09-09 14:48:53 +0200

    pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix=[]
+    new_matrix = []
     for ligne in matrix:
         nouvelle_ligne = []
         for valeur in ligne:
