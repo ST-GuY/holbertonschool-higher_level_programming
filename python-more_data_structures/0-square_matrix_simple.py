9e018404083cee71aa9b6effe741bfc3799ca420
ST-GuY <estebanlvy23@gmail.com>
2025-09-09 14:45:52 +0200

    matrix simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix=[]
+    for ligne in matrix:
+        nouvelle_ligne = []
+        for valeur in ligne:
+            nouvelle_ligne.append(valeur * valeur)
+        new_matrix.append(nouvelle_ligne)
+    return new_matrix
