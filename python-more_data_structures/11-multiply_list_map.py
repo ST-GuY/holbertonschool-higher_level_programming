be3d95c71fd6f9b2e674137d1a1712a02d53176b
ST-GuY <estebanlvy23@gmail.com>
2025-09-10 14:28:18 +0200

    commentaire

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
+def multiply_list_map(my_list=[], number=0):  # Définition de la fonction avec une liste et un nombre par défaut
+    # map applique une fonction (ici lambda) à chaque élément de my_list
+    # lambda x: x * number -> prend chaque élément x et le multiplie par number
+    # list(...) transforme l'objet map en une nouvelle liste (sans modifier my_list)
     return list(map(lambda x: x * number, my_list))
