70b46c819153c6cba8e2e8fc647fa13475b61eff
ST-GuY <estebanlvy23@gmail.com>
2025-09-12 11:03:48 +0200

    list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,4 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
+        finally:
+            pass
     return result
