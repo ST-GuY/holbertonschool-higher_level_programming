4f478b4e4f4c59eaf97137e680bb2fc307ce2469
ST-GuY <estebanlvy23@gmail.com>
2025-09-12 09:36:16 +0200

    safe print list integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-	
+    count = 0
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break  # On arrête si on dépasse la longueur de la liste
+        except (ValueError, TypeError):
+            pass  # On ignore les éléments qui ne sont pas des entiers
+        finally:
+            # On incrémente i dans tous les cas pour éviter une boucle infinie
+            i += 1
+    print()
+    return count
