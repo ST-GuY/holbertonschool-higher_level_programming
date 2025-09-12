f80e832c77d59c3de60688025df65734abd126cf
ST-GuY <estebanlvy23@gmail.com>
2025-09-12 11:02:30 +0200

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,14 +3,17 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            is_elem1_number = isinstance(my_list_1[i], (int, float))
+            is_elem2_number = isinstance(my_list_2[i], (int, float))
+            if not is_elem1_number or not is_elem2_number:
+                print("wrong type")
+                result.append(0)
+            else:
+                result.append(my_list_1[i] / my_list_2[i])
         except IndexError:
             print("out of range")
             result.append(0)
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
-        except (TypeError, ValueError):
-            print("wrong type")
             result.append(0)
     return result
