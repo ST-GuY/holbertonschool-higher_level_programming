5039d5ac4633b3acbc6b37ccd48dd29a0b847645
ST-GuY <estebanlvy23@gmail.com>
2025-09-12 16:44:33 +0200

    doc function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,12 @@ le prénom et le nom d'une personne.
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Affiche 'My name is <first_name> <last_name>'.
+
+    first_name et last_name doivent être des chaînes de caractères.
+    Lève TypeError si l'un des paramètres n'est pas une chaîne.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
