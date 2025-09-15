71a3b6819687c7bed672fafae19ffd5729dcface
ST-GuY <estebanlvy23@gmail.com>
2025-09-15 16:02:47 +0200

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+class Square:
+    """Classe qui définit un carré avec un attribut privé size."""
+
+    def __init__(self, size):
+        """Initialise un carré avec une taille donnée.
+        Aucun contrôle de type ou de valeur n’est effectué.
+        """
+        self.__size = size
