931273a8aed7ffba312385866b1b11d5b1b9d30c
ST-GuY <estebanlvy23@gmail.com>
2025-09-22 11:08:19 +0200

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class MyList(list):
+    """Une sous-classe de list avec une méthode supplémentaire pour afficher la liste triée."""
+
+    def print_sorted(self):
+        """Affiche la liste dans l’ordre croissant."""
+        print(sorted(self))
