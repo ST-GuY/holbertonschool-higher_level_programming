c1212abf85ef1c657e4e6a6afcefd66268cf5e9d
ST-GuY <estebanlvy23@gmail.com>
2025-09-24 14:10:24 +0200

    add abs

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -20,7 +20,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return abs(math.pi * self.radius ** 2)
 
     def perimeter(self):
         return 2 * math.pi * self.radius
