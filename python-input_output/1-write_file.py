f38a6016ab149672db95db7d8f87ba557885012a
ST-GuY <estebanlvy23@gmail.com>
2025-09-29 11:37:17 +0200

    doc

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module 1-write_file: contient la fonction write_file
+qui écrit une chaîne dans un fichier UTF-8 et
+retourne le nombre de caractères écrits."""
+
+
 def write_file(filename="", text=""):
     """Écrit une chaîne de caractères dans un fichier UTF-8
     et retourne le nombre de caractères écrits"""
