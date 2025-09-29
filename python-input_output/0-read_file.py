1e8e328c5e92a7a0a31c7bd8467ca9e8af745880
ST-GuY <estebanlvy23@gmail.com>
2025-09-29 11:27:55 +0200

    doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module 0-read_file: contient la fonction read_file qui lit un fichier UTF-8 et affiche son contenu."""
+
+
 def read_file(filename=""):
+    """Lit un fichier UTF-8 et affiche son contenu"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read, end="")
+        print(f.read(), end="")
