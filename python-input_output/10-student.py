b6fd32226d73f2b1317f4cff88d9631c1d1a1e7c
ST-GuY <estebanlvy23@gmail.com>
2025-10-04 18:20:49 +0200

    doc

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Classe qui définit un étudiant avec son prénom, son nom et son âge.
+Définit la classe Student avec des attributs publics et une méthode to_json().
 """
 
 
@@ -9,15 +9,36 @@ class Student:
     Classe qui définit un étudiant avec son prénom, son nom et son âge.
     """
 
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialise un nouvel étudiant.
 
-def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        Args:
+            first_name (str): Le prénom de l'étudiant.
+            last_name (str): Le nom de famille de l'étudiant.
+            age (int): L'âge de l'étudiant.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
+    def to_json(self, attrs=None):
+        """
+        Retourne une représentation dictionnaire (JSON) de l'objet Student.
 
-def to_json(self, attrs=None):
-    if attrs is None:
-        return self.__dict__
-    else:
-        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        Si `attrs` est une liste de chaînes de caractères, seule la liste des
+        attributs dont le nom est présent dans cette liste sera retournée.
+
+        Sinon, tous les attributs de l'objet seront retournés.
+
+        Args:
+            attrs (list, optionnel): Liste de noms d'attributs à inclure.
+                                     Si None, tous les attributs sont inclus.
+
+        Returns:
+            dict: Dictionnaire contenant les paires clé/valeur de l'étudiant.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
