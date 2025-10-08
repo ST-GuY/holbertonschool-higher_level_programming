f0bba4a67277106db3e3c39678bb76001e0b108b
ST-GuY <estebanlvy23@gmail.com>
2025-10-08 16:21:18 +0200

    server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,5 +8,5 @@ PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port" PORT)
+    print("serving at port", PORT)
     httpd.serve_forever()
