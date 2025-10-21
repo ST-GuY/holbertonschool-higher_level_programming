09b01c8e821cf44ee4466857fa58a0ff8c710127
ST-GuY <estebanlvy23@gmail.com>
2025-10-21 15:03:55 +0200

    top score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Script qui liste tous les enregistrements de second_table
+-- Affiche le score et le nom, triés par score décroissant
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
