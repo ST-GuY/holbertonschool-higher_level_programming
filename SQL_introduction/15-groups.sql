bd821c10ed5a2a1d73120c128fc0e3d6d7a5a0ac
ST-GuY <estebanlvy23@gmail.com>
2025-10-21 16:16:21 +0200

    number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Compter le nombre d'enregistrements par score
-SELECT score, COUNT(*) AS count
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY count DESC;
+ORDER BY number DESC;
