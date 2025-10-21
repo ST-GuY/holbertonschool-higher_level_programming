074917347a37571b5aee8cf746770207f8fa1539
ST-GuY <estebanlvy23@gmail.com>
2025-10-21 14:58:14 +0200

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,12 @@
-CREATE TABLE second_table (
+-- Script qui crée une table second_table et insère plusieurs lignes
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
