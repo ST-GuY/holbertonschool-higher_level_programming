9e735b3092a7cd2c470e46ba2db80041a07c6b9b
ST-GuY <estebanlvy23@gmail.com>
2025-10-22 11:46:11 +0200

    alter user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
--- 1-create_user.sql
+-- Crée l'utilisateur si il n'existe pas
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' BY 'user_0d_1_pwd';
+-- Met à jour le mot de passe au cas où l'utilisateur existait déjà
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
