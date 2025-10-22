c6129d83252142716b64ab9f02388c76fdb017e3
ST-GuY <estebanlvy23@gmail.com>
2025-10-22 11:49:16 +0200

    syntaxe

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Crée l'utilisateur si il n'existe pas
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' BY 'user_0d_1_pwd';
 -- Met à jour le mot de passe au cas où l'utilisateur existait déjà
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' BY 'user_0d_1_pwd';
 -- Donne tous les privilèges sur toutes les bases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
