7dba6974d6ef30f2c1b74d8437ec6f220a592386
ST-GuY <estebanlvy23@gmail.com>
2025-10-23 09:25:07 +0200

    cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities in hbtn_0d_usa with the format:
-SELECT CONCAT(cities.id, ' - ', cities.name, ' - ', states.name) AS city_info
-FROM cities, states
-WHERE cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
