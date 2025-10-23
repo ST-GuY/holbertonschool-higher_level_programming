ed5f61314866cfa76125aeb6115854872a500bec
ST-GuY <estebanlvy23@gmail.com>
2025-10-23 09:42:44 +0200

    genre id

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Importer le dump de la base de données de hbtn_0d_tvshows sur votre serveur MySQL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
