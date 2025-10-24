db472d20785476edaef5ef75c2317a9793b7e559
ST-GuY <estebanlvy23@gmail.com>
2025-10-24 14:51:25 +0200

    select states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Création d’un curseur pour exécuter les requêtes
+    cur = db.cursor()
+
+    # Exécution de la requête SQL
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Récupération et affichage de tous les résultats
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Fermeture du curseur et de la connexion
+    cur.close()
+    db.close()
