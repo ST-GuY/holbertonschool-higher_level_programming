cd01eeba23e8de4477e3f02c850dca3360c93866
ST-GuY <estebanlvy23@gmail.com>
2025-11-09 12:12:57 +0100

    value 89

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// On remplace la valeur 12 par 89
+myObject.value = 89;
+
+console.log(myObject);
