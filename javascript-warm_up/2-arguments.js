2879c0dcd7c472053ccdd48861ccf824204a7fa7
ST-GuY <estebanlvy23@gmail.com>
2025-11-09 11:26:06 +0100

    arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// On calcule le nombre d'arguments passés par l'utilisateur
+// process.argv contient tous les arguments :
+// [0] -> chemin vers node
+// [1] -> chemin vers le script
+// [2..n] -> arguments réels passés
+const argsCount = process.argv.length - 2;
+
+// On vérifie combien d'arguments ont été passés
+if (argsCount === 0) {
+  // Aucun argument passé
+  console.log('No argument');
+} else if (argsCount === 1) {
+  // Un seul argument passé
+  console.log('Argument found');
+} else {
+  // Deux arguments ou plus
+  console.log('Arguments found');
+}
