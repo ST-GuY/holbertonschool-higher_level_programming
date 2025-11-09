ba86e6ac71cd5c818b4cb5ba6cc2d02d8c70b79f
ST-GuY <estebanlvy23@gmail.com>
2025-11-09 11:44:20 +0100

    loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Tableau contenant les lignes à afficher
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Affichage avec une seule instruction console.log et une boucle implicite via join
+console.log(languages.join('\n'));
+
