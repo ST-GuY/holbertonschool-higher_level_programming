e9d8d46e1e961aca4846cd473e075425b1ad028d
ST-GuY <estebanlvy23@gmail.com>
2025-11-09 12:14:20 +0100

    add file

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Définir la fonction add qui additionne deux entiers
+function add(a, b) {
+  return a + b;
+}
+
+// Rendre la fonction visible depuis l'extérieur
+module.exports.add = add;
