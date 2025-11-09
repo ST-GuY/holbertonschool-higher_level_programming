90c4f89fe684e9f81d04d7e555e511aeedede4d4
ST-GuY <estebanlvy23@gmail.com>
2025-11-09 11:48:45 +0100

    sans if

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
-// Tableau contenant les lignes à afficher
+// Tableau des lignes
 const languages = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
 ];
 
-// Construire la chaîne finale avec une boucle
-let output = ''; // chaîne temporaire pour stocker le résultat
+// Construire un tableau temporaire pour ajouter les retours à la ligne
+const output = [];
 for (let i = 0; i < languages.length; i++) {
-  output += languages[i]; // ajouter la ligne
-  if (i !== languages.length - 1) {
-    output += '\n'; // ajouter un retour à la ligne sauf pour la dernière ligne
-  }
+  output.push(languages[i]); // ajouter chaque ligne
 }
 
-console.log(output);
+// Afficher tout avec une seule console.log
+console.log(output.join('\n'));
 
