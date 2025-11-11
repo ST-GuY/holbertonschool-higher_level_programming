f2721e432948982041a800dee78db98f2b04abd7
ST-GuY <estebanlvy23@gmail.com>
2025-11-11 14:26:34 +0100

    star wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results; // Liste des films
+    const list = document.getElementById('list_movies');
+
+    movies.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      list.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Erreur :', error);
+  });
